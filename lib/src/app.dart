da13850c660d4408d1f88547df40e3afcf780804
keshav2010 <sharmakeshav15157@gmail.com>
2023-05-17 00:20:29 +0530

    basic flutter app flow

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
 import 'package:veil/src/pages/profile.dart';
 import 'package:veil/src/pages/register.dart';
-import 'package:veil/src/pages/post.dart';
+import 'package:veil/utils/auth/auth.dart' as auth;
 
 class App extends StatelessWidget {
   const App({Key? key});
@@ -16,7 +12,7 @@ class App extends StatelessWidget {
       title: 'Veil',
       theme: ThemeData(
         primarySwatch: Colors.teal,
-        scaffoldBackgroundColor: Color.fromARGB(255, 187, 180, 212),
+        scaffoldBackgroundColor: Color.fromARGB(255, 201, 192, 236),
       ),
       home: const HomePage(title: 'Veil'),
     );
@@ -36,69 +32,25 @@ class _HomePageState extends State<HomePage> {
   bool _loading = false;
   bool _error = false;
 
-  void _checkToken() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? token = prefs.getString('token');
-    if (token != null) {
-      setState(() {
-        _loading = true;
-      });
-      try {
-        // make network call here to validate token
-        // if token is valid, move to post.dart page
-        // otherwise, move to register page
-        setState(() {
-          _loading = false;
-        });
-      } catch (e) {
-        setState(() {
-          _loading = false;
-          _error = true;
-        });
-      }
-    } else {
-      setState(() {
-        _showRegisterScreen = true;
-      });
-    }
-  }
-
-  void _saveToken() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    try {
-      // make network call here to create token
-      await prefs.setString('token', '12356');
-      setState(() {
-        _loading = false;
-      });
-    } catch (e) {
-      setState(() {
-        _loading = false;
-        _error = true;
-      });
-    }
-  }
-
   @override
   void initState() {
     super.initState();
-    _checkToken();
   }
 
   @override
   Widget build(BuildContext context) {
     if (_error) {
-      return Scaffold(
+      return const Scaffold(
         body: Center(
           child: Text(
-            'Error connecting to services',
+            'Error connecting to services.',
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
       );
     }
     if (_loading) {
-      return Scaffold(
+      return const Scaffold(
         body: Center(
           child: CircularProgressIndicator(),
         ),
@@ -113,13 +65,21 @@ class _HomePageState extends State<HomePage> {
               const RegisterPage(),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  _saveToken();
-                  setState(() {
-                    _loading = true;
-                  });
+                onPressed: () async {
+                  try {
+                    setState(() {
+                      _loading = true;
+                    });
+                    var storedToken = await auth.findTokenOnDevice();
+
+                    //validate the stored token
+                    print('found token in device(${storedToken}), validating it.');
+                  }
+                  on Exception catch (_, e) {
+                    setState(() { _loading = true; });
+                  }
                 },
-                child: const Text("Let's go!"),
+                child: const Text("Login As New User!"),
               ),
             ],
           ),
